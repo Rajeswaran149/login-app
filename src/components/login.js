3ac21ca9f48ac40534d644a1f4797d44187a5768
Rajeswaran <rajeswaran149@gmail.com>
2024-06-25 14:01:40 +0530

    update form validation

--- src/components/login.js
+++ src/components/login.js
@@ -10,7 +10,10 @@ function Login() {
  const [email , setEmail] = useState('')
  const [password , setPassword] = useState('')
  const [ loggedin , setLoggedin ] = useState(false)
- const [ error , setError ] = useState('')
+ const [ error , setError ] = useState({})
+ const [showErrorMessage , setShowErrorMessage] = useState(false)
+ 
+
 
 
   const handleSubmit = async (e) => {
@@ -22,16 +25,42 @@ function Login() {
 
       if (credentials) {
         setLoggedin(true)
+      
+
       } else {
         setError("Authentication failed. Please check your credentials.");
+        setShowErrorMessage(true);
+        setTimeout(() => {
+          setShowErrorMessage(false)
+        }, 3000);
       }
 
     }catch (error) {  
       console.error("login failed : " , error.message);
+      setShowErrorMessage(true);
+      setTimeout(() => {
+        setShowErrorMessage(false)
+      }, 3000);
     }
 
-    console.log("email :" ,email);
-    console.log("password :",password);
+    const error = {};
+    if(!email.trim()) {
+      error.email = 'email is required'
+    }
+
+    if(!password.trim()) {
+      error.password = "Password is required"
+    }
+    
+    if(Object.keys(error).length > 0){
+      setError(error);
+      setTimeout(() => {
+        setError({});
+      }, 3000);
+      return;
+    }
+    // console.log("email :" , email);
+    // console.log("password :", password);
   }
 
   if (loggedin) {
@@ -44,9 +73,12 @@ function Login() {
         <form className='form-group' onSubmit={handleSubmit}>
           <p className='paragraph'>Login here !!!</p>
             <input className='input-field' type='text' placeholder='email' id='' name='email' value={email} onChange={(e) =>setEmail(e.target.value)}></input><br></br>
+            {error.email && <p className="error-message">{error.email}</p>}
             <input type='password' className='input-field' placeholder='password' id='' name='password' value={password} onChange={(e) => setPassword(e.target.value)}></input><br></br>
-            {/* {error && <p className="error-message">{error}</p>} */}
-            <button className='btn' type='submit'>Login</button>
+            {error.password && <p className="error-message">{error.password}</p>}
+            <button className='btn' type='submit' >Login</button>
+            {showErrorMessage && <p className='error-message'>invalid credentials</p>}
+        
           <p className='paragraph'>create your account here...  <Link className='anchor' to='/signup'>Signup</Link></p>
         </form>
       </div>
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './signup.css'
 import axios from 'axios';
-import { Link, Navigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 
 export default function Signup() {
 
@@ -10,30 +10,72 @@ export default function Signup() {
   const [ password , setPassword ] = useState('');
   const [ signup , setSignupin ] = useState(false);
   const [ error , setError ] = useState('')
+  const [ showSignupErrorMessage , setShowSignupErrorMessage ] = useState(false)
+  const [ showRegisteredMessage , setShowRegisteredMessage ] = useState(false)
 
   const handleSubmit = (async (e) => {
     e.preventDefault()
+
+    const error = {};
+    if(!username){
+      error.username = 'Username is required '
+    }
+    if(!email || email === "example@gmail.com"){
+      error.email = ' Email id is required '
+    }
+
+    if(!password){
+      error.password = 'Password is required'  
+      }
+
+    if(Object.keys(error).length > 0){
+      setError(error)
+      setTimeout(() => {
+        setError({})
+      }, 3000);
+    }
+
     try {
      const credentials = await axios.post('https://login-api-iota.vercel.app/api/auth/signup' , { username ,email , password });
      console.log('credential :' , credentials);
      
      if (credentials) {
       setSignupin(true)
+      setShowRegisteredMessage(true)
+      setTimeout(() => {
+        setShowRegisteredMessage(false)
+      }, 3000);
      } else {
       setError("check your credentials")
+      setShowSignupErrorMessage(true);
+      setTimeout(() => {
+        setShowSignupErrorMessage(false);
+      }, 3000);
      }
 
     } catch (error){
       console.error('signup failed :' , error.message )
+      setError("email id is already used ")
+      setShowSignupErrorMessage(true);
+      setTimeout(() => {
+        setShowSignupErrorMessage(false);
+      }, 3000);
+      
     }
-    console.log( 'username :' , username );
-    console.log( 'email :' , email );
-    console.log( 'password :' , password );
+    // console.log( 'username :' , username );
+    // console.log( 'email :' , email );
+    // console.log( 'password :' , password );
+    
+   
+    setUsername('');
+    setEmail('');
+    setPassword('')
   })
 
-  if(signup) {
-    return <Navigate to = "/" />
-  }
+  // if(signup) {
+  //   return <Navigate to = "/" />
+  // }
+ 
 
   return (
     <div className='container'>
@@ -41,9 +83,14 @@ export default function Signup() {
           <form className='form-group' onSubmit={handleSubmit} >
               <p className='paragraph'>Signup here..!!!</p>
               <input className='input-field' placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)}/><br />
+              {error.username && <p className='error-message'>{error.username}</p>}
               <input className='input-field' type='text' placeholder='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)}  /><br />
+              {error.email && <p className='error-message'>{error.email}</p>}
               <input className='input-field' type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} /> <br />  
+              {error.password && <p className='error-message'>{error.password}</p>}
               <button className='btn' type='submit'>Submit</button>
+              { showRegisteredMessage && <p className='succes-message'>Registered succesful</p>}
+              { showSignupErrorMessage && <p className='error-message'>check your credentials </p> }
               <p className='paragraph'>Login your account here.. <Link className='anchor' to='/'>Login</Link></p>
           </form>
       </div>
