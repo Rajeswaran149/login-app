ce731f2364ed0851f5e2ee1d4d64c78f5cbc54f7
Rajeswaran <rajeswaran149@gmail.com>
2024-06-27 12:22:12 +0530

    update "pagination"

--- src/components/dataTable1.js
+++ src/components/dataTable1.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import "./dataTable1"
+import "./dataTable1.css"
 export default function DataTable1() {
 
-    const [ data , setData ] = useState([])
+    const [ data , setData ] = useState([]);
+    const [ currentPage , setCurrentPage ] = useState(1);
+    const [ itemsPerPage ] = useState(10);
 
   
     useEffect (()=>{
@@ -26,34 +28,49 @@ export default function DataTable1() {
            }
       })
 
+      const lastIndexOfItem = currentPage * itemsPerPage;
+      const firstIndexOfItem = lastIndexOfItem - itemsPerPage;
+      const currentItems = data.slice( firstIndexOfItem , lastIndexOfItem );
+
+      const paginate = (pageNumber) =>{
+        setCurrentPage(pageNumber)
+      }
 
   return (
     <div className='table-container'>
-    <h2>Listed data from server</h2>
-    <table className='data-table'>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Brand</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Description</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                data.map((e)=> (
-                    <tr key={e.id}>
-                       <td>{e.id}</td>
-                       <td>{e.brand}</td>
-                       <td>{e.name}</td>
-                       <td>{e.category}</td>
-                       <td>{e.description}</td>
-                    </tr>
-                ))
-            }
-        </tbody>
-    </table>
+      <h2>Listed data from server</h2>
+      <table className='data-table'>
+          <thead>
+              <tr>
+                  <th>Id</th>
+                  <th>Brand</th>
+                  <th>Name</th>
+                  <th>Category</th>
+                  <th>Description</th>
+              </tr>
+          </thead>
+          <tbody>
+              {
+                  currentItems.map((e)=> (
+                      <tr key={e.id}>
+                        <td>{e.id}</td>
+                        <td>{e.brand}</td>
+                        <td>{e.name}</td>
+                        <td>{e.category}</td>
+                        <td>{e.description}</td>
+                      </tr>
+                  ))
+              }
+          </tbody>
+      </table>
+      <div className='pagination'>
+        <button className='pagination-button' onClick={ () => paginate(1) } disabled = {currentPage === 1 }>First</button>
+        <button className='pagination-button' onClick={ () => paginate( currentPage - 1 )} disabled = { currentPage === 1 } >Previous</button>
+        <span className='pagination-current-page'>{currentPage}</span>
+        <button className='pagination-button' onClick={ () => paginate( currentPage + 1 ) } disabled = { currentPage === Math.ceil(data.length / itemsPerPage) }>Next</button>
+        <button className='pagination-button' onClick={ () => paginate(Math.ceil( data.length / itemsPerPage ))} disabled = { currentPage === Math.ceil(data.length/itemsPerPage)}>Last</button>
+      </div>
+
     </div>
   )
 }
